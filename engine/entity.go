83b9daf075b4776fe254ce1d6b31ac420d966279
Nick Bryan <nickbryan.dev@gmail.com>
2019-03-10 11:57:51 +0000

    Adding entity tests and documentation.

--- engine/entity.go
+++ engine/entity.go
@@ -1,23 +1,38 @@
 package engine
 
+// Entity represents a single entity/world object managed by the engine. An Entity is identified by its unique Id.
+//
+// Due to this engines implementation of an Entity Component System, the Entity does not know about the components
+// that are associated with it. Instead, an Entity should be registered with a component manager and it should be
+// up to that component manager to keep track of the data relevant to the Entity within that system.
 type Entity struct {
 	Id uint64
 }
 
+// EntityManager is responsible for the creation and destruction of Entities within the engine. All Entities should
+// be created by the EntityManager to ensure that they are managed correctly.
 type EntityManager struct {
 	entities map[uint64]*Entity
 }
 
+// NewEntityManager will create a new EntityManager.
 func NewEnitytManager() *EntityManager {
 	return &EntityManager{
 		entities: make(map[uint64]*Entity),
 	}
 }
 
-func (em *EntityManager) CreateEntity() *Entity {
-	e := &Entity{}
+// Create will iterate over the existing Entities until it finds a slot in which an entity does not exist. It will
+// then add the Entity to the entities list and return the newly created Entity.
+//
+// Entity Ids start at 1.
+//
+// Managing Entities this way allows us to ensure that there are no gaps in our Entity Id's and keeps our map sequentially
+// stored in memory.
+func (em *EntityManager) Create() *Entity {
+	e := &Entity{Id: 1}
 
-	for em.IsAlive(e) {
+	for em.Alive(e) {
 		e.Id++
 	}
 
@@ -26,11 +41,13 @@ func (em *EntityManager) CreateEntity() *Entity {
 	return e
 }
 
-func (em *EntityManager) IsAlive(e *Entity) bool {
+// Alive will check to see if there is an Entity mapped with the given Entities Id within the EntityManager.
+func (em *EntityManager) Alive(e *Entity) bool {
 	_, ok := em.entities[e.Id]
 	return ok
 }
 
+// Destroy will remove the given Entity from the EntityManager.
 func (em *EntityManager) Destroy(e *Entity) {
 	delete(em.entities, e.Id)
 }
---
+++ engine/entity_test.go
@@ -0,0 +1,66 @@
+package engine
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+	em := NewEnitytManager()
+
+	e1 := em.Create()
+	e2 := em.Create()
+	e3 := em.Create()
+
+	if e1.Id != 1 {
+		t.Errorf("Entity1 Id expected to be 1, but received: %v", e1.Id)
+	}
+
+	if e2.Id != 2 {
+		t.Errorf("Entity2 Id expected to be 2, but received: %v", e2.Id)
+	}
+
+	if e3.Id != 3 {
+		t.Errorf("Entity3 Id expected to be 3, but received: %v", e3.Id)
+	}
+
+	em.Destroy(e2)
+	e4 := em.Create()
+	if e4.Id != 2 {
+		t.Errorf("Entity4 expected to have filled slot 2 with Id expected to be 2, but received Id: %v", e4.Id)
+	}
+}
+
+func TestAlive(t *testing.T) {
+	em := NewEnitytManager()
+
+	e1 := em.Create()
+	e2 := em.Create()
+	e3 := &Entity{Id: 3}
+
+	if em.Alive(e1) == false {
+		t.Error("Entity1 was expected to be alive but alive returned false")
+	}
+
+	if em.Alive(e2) == false {
+		t.Error("Entity2 was expected to be alive but alive returned false")
+	}
+
+	if em.Alive(e3) == true {
+		t.Error("Entity3 was not expected to be alive but alive returned true")
+	}
+}
+
+func TestDestroy(t *testing.T) {
+	em := NewEnitytManager()
+
+	e1 := em.Create()
+	e2 := em.Create()
+
+	em.Destroy(e2)
+
+	if em.Alive(e1) == false {
+		t.Error("Entity1 was expected to be alive but alive returned false")
+	}
+
+	if em.Alive(e2) == true {
+		t.Error("Entity2 was not expected to be alive but alive returned true")
+	}
+}
